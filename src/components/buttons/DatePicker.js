ec57fe1d55a9a68e0dea8a339ee43768f50d0e55
Detox23 <jakub23sa@wp.pl>
2021-02-16 20:32:14 +0100

    Change navigations

--- src/components/buttons/DatePicker.js
+++ src/components/buttons/DatePicker.js
@@ -4,9 +4,9 @@ import {DatePickerButton} from './DatePickerButton';
 export const DatePicker = ({formatDate, getDayName, getDate, date}) => {
     return (        
         <div  className="ui column centered grid">
-            <DatePickerButton action={() => getDate(true)} visibleContent={"Wczorajsze zadania"} hiddenContent={<i className="left arrow icon"></i>}/>
+            <DatePickerButton action={() => getDate(false)} visibleContent={"Wczorajsze zadania"} hiddenContent={<i className="left arrow icon"></i>}/>
             <DatePickerButton action={() => function(){}} visibleContent={getDayName(date)} hiddenContent={formatDate(date)}/>
-            <DatePickerButton action={() => getDate(false)} visibleContent={"Jutrzejsze zadania"} hiddenContent={<i className="right arrow icon"></i>}/>
+            <DatePickerButton action={() => getDate(true)} visibleContent={"Jutrzejsze zadania"} hiddenContent={<i className="right arrow icon"></i>}/>
         </div>
     )
 }
