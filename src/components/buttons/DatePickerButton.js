a0715e290ecd68bc592c15328fc46ea02baa0983
Detox23 <jakub23sa@wp.pl>
2021-02-17 00:04:38 +0100

    Fix local run and date navigators

--- src/components/buttons/DatePickerButton.js
+++ src/components/buttons/DatePickerButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const DatePickerButton =({action, visibleContent, hiddenContent}) => {
     return (               
-        <div onClick={() => action()} className="ui animated fade button" tabIndex="0">
+        <div onClick={() => action()} className="ui animated fade button dateButtonsSize" tabIndex="0">
             <div className="visible content">{visibleContent}</div>
             <div className="hidden content">
                 {hiddenContent}
